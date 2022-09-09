00759a0b53c519b6ac749c4106e2ed05d0e9f169
YasiruLak <yasirudahanayaka19@gmail.com>
2022-09-09 10:56:08 +0530

    Add Components

---
+++ Abc.js
@@ -0,0 +1,12 @@
+import React from "react";
+import{ View,Text } from "react-native";
+
+function Abc(){
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    );
+}
+
+export default Abc;
---
+++ XXX.js
@@ -0,0 +1,14 @@
+import React from "react";
+import{ View,Text } from "react-native";
+import Abc from "./Abc";
+
+function XXX(){
+    return(
+        <View>
+            <Text>Xxx</Text>
+            <Abc/>
+        </View>
+    );
+}
+
+export default XXX;
